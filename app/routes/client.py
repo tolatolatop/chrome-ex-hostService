5a080c1462673d9277cee4d82562f19a4608915f
tolatolatop <tolatolatop@gmail.com>
2025-04-13 13:48:58 +0000

    feat: 抛弃其他请求

--- app/routes/client.py
+++ app/routes/client.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from ..channel import channel, Message
+from ..channel import channel, Message, ResponseMessage
+import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -11,12 +14,21 @@ async def websocket_endpoint(websocket: WebSocket, uid: str):
     while True:
         try:
             message = await channel.receive_request(uid)
-            await websocket.send_json(message.data)
-            response = await websocket.receive_json()
-            await channel.send_response(uid, message.request_id, response)
+            data = message.model_dump()
+            logger.info("receive request %s %s", uid, data)
+            await websocket.send_json(data)
+            while True:
+                response = await websocket.receive_json()
+                response_message = ResponseMessage(**response)
+                if response_message.request_id != message.request_id:
+                    logger.info("drop response %s %s", uid,
+                                response_message.data)
+                    continue
+                logger.info("send response %s %s", uid, response_message.data)
+                await channel.send_response(uid, message.request_id, response_message.data)
         except WebSocketDisconnect:
             break
         except Exception as e:
-            print(e)
+            logger.error(f"Error: {e}")
             break
     channel.destory_channel(uid)
