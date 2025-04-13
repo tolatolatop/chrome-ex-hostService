355649607b358e56adc0fd1e163d7118c70e5ea6
tolatolatop <tolatolatop@gmail.com>
2025-04-13 02:17:15 +0000

    feat: 初始化数据库

--- app/models/database.py
+++ app/models/database.py
@@ -1,11 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./school.db"
+# 默认使用内存中的SQLite数据库
+DEFAULT_DATABASE_URL = "sqlite:///:memory:"
+
+# 从环境变量获取数据库URL，如果没有则使用默认值
+SQLALCHEMY_DATABASE_URL = os.getenv("SQL_URI", DEFAULT_DATABASE_URL)
+
+# 根据数据库类型设置不同的连接参数
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args=connect_args,
+    pool_pre_ping=True  # 启用连接池健康检查
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
