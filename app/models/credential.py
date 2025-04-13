b61f40a94fc46fef4d6774f6ccbdde33176b7436
tolatolatop <tolatolatop@gmail.com>
2025-04-13 02:48:08 +0000

    feat: 支持管理凭据

---
+++ app/models/credential.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+from app.models.database import Base
+import enum
+
+
+class CredentialType(enum.Enum):
+    API_KEY = "api_key"
+    ACCESS_TOKEN = "access_token"
+    SECRET_KEY = "secret_key"
+    PASSWORD = "password"
+    OTHER = "other"
+    COOKIES = "cookies"
+
+
+class Credential(Base):
+    __tablename__ = "credentials"
+
+    id = Column(Integer, primary_key=True, index=True)
+    owner = Column(String, index=True, nullable=False)
+    credential = Column(String, nullable=False)
+    credential_type = Column(Enum(CredentialType),
+                             nullable=False, default=CredentialType.OTHER)
+    expire_time = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow,
+                        onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Credential(id={self.id}, owner={self.owner}, type={self.credential_type}, expire_time={self.expire_time})>"
